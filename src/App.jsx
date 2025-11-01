e28c290d83c2c54439ce113b8e16907f68b422fa
Alireza-molaei <alirezamolaei3013@gmail.com>
2025-11-01 15:41:29 +0330

    step 3

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Prduct from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="product" element={<Prduct />}></Route>
+        <Route path="pricing" element={<Pricing />}></Route>
+        <Route path="login" element={<Login />}></Route>
+
+        <Route path="app" element={<AppLayout />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return <AppNav />;
+}
+
+export default AppLayout;
