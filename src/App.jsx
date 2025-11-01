9046729686081167c63728313853e37cb61f6d06
Alireza-molaei <alirezamolaei3013@gmail.com>
2025-11-01 16:30:17 +0330

    nested route for sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />}></Route>
+        <Route index element={<Homepage />}></Route>
         <Route path="product" element={<Prduct />}></Route>
         <Route path="pricing" element={<Pricing />}></Route>
         <Route path="login" element={<Login />}></Route>
 
         <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of citires</p>}></Route>
           <Route path="cities" element={<p>list of cities</p>}></Route>
-          <Route path="conytries" element={<p>contries</p>}></Route>
+          <Route path="countries" element={<p>contries</p>}></Route>
+          <Route path="form" element={<p>Form</p>}></Route>
         </Route>
 
         <Route path="*" element={<PageNotFound />}></Route>
--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import styles from "./AppNav.module.css";
+import { NavLink } from "react-router-dom";
 function AppNav() {
-  return <nav className={styles.nav}>AppNav</nav>;
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+
+        <li>
+          <NavLink to="countries">countries</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default AppNav;
