ed10ccc41ca87878ef0c31d3f84aa63beb77ea80
Alireza-molaei <alirezamolaei3013@gmail.com>
2025-11-01 16:00:55 +0330

    add map and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,11 @@ function App() {
         <Route path="pricing" element={<Pricing />}></Route>
         <Route path="login" element={<Login />}></Route>
 
-        <Route path="app" element={<AppLayout />}></Route>
+        <Route path="app" element={<AppLayout />}>
+          <Route path="cities" element={<p>list of cities</p>}></Route>
+          <Route path="conytries" element={<p>contries</p>}></Route>
+        </Route>
+
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
     </BrowserRouter>
